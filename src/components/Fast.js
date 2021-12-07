44f00e500d4e66827b6c198a245730ec283ab8b4
jydhasan <s1910933181@ru.ac..bd>
2021-12-07 18:08:57 +0600

    Initial commit

---
+++ src/components/Fast.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Fast = () => {
+    return (
+        <div>
+            <div className="navbar">
+                <h1>
+                    My First React application
+                </h1>
+            </div>
+            <p className="fastpara">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aut, amet vel aperiam aliquam repudiandae expedita, consequuntur libero, molestiae rerum quasi fuga! Non nemo fugit vitae soluta facilis nihil minima.
+                Suscipit minima laborum aperiam molestias ipsa necessitatibus perspiciatis expedita magnam totam facilis commodi dicta doloremque maiores, optio iure officia adipisci voluptatem amet modi porro ipsam incidunt dolor? Iure, sequi consequatur!
+                Nulla praesentium corporis maxime quia assumenda natus, ipsum cumque quis, id voluptates suscipit. Tempora inventore alias minus ab! Cum suscipit impedit inventore repudiandae similique minus velit non nobis dolor ipsum?
+                Doloribus quidem unde inventore ratione consectetur iusto consequatur ipsam, quisquam fuga eius, accusantium explicabo ipsa molestiae facilis sint expedita incidunt eveniet saepe quis! Repudiandae consectetur molestiae minima, cupiditate autem eos.
+            </p>
+        </div>
+    )
+}
+
+export default Fast
